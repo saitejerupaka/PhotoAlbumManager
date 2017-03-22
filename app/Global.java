cdeb2c33bbb9521157839f6e3222f88c2e00d74f
Saitej Erupaka <serupaka@gmail.com>
2017-03-21 22:37:52 -0500

    Global Exception Log and Bar Request Log

--- app/Global.java
+++ app/Global.java
@@ -2,6 +2,10 @@ import dataMappers.AlbumMapper;
 import dataMappers.PhotoMapper;
 import play.Application;
 import play.GlobalSettings;
+import play.Logger;
+import play.libs.F;
+import play.mvc.Http;
+import play.mvc.Result;
 
 
 public class Global extends GlobalSettings {
@@ -11,4 +15,21 @@ public class Global extends GlobalSettings {
         AlbumMapper.initializeAlbums();
         PhotoMapper.initializePhotos();
     }
+
+    @Override
+    public F.Promise<Result> onError(Http.RequestHeader request, Throwable t) {
+        String message = String.format("Server Error while processing request - %s ", request.uri());
+
+        Logger.error(message, t);
+        return super.onError(request, t);
+    }
+
+    @Override
+    public F.Promise<Result> onBadRequest(Http.RequestHeader request, String error) {
+        String message = String.format("Cannot process request- %s,  error- %s} ", request.uri(), error);
+        Logger.info(message);
+        return super.onBadRequest(request, error);
+    }
+
+
 }
